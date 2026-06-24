da09c38c788f583a768c72578573ea45250b8cfa
kire-aleph <e.broecker+github@mailbox.org>
2026-06-24 11:35:03 +0200

    schneller und auch mit delay noch funktionsfähig

--- test/ZZA_200x16rows_x4_full_test/ZZA_200x16rows_x4_full_test.ino
+++ test/ZZA_200x16rows_x4_full_test/ZZA_200x16rows_x4_full_test.ino
@@ -3,19 +3,21 @@
 #include <stdint.h>
 #include "imageData.h"
 
-// Pin 01   D_G         red
-// Pin 03   D_R         green
-// Pin 05   CLK         orange
-// Pin 07   STR         yellow
-// Pin 09   A0          white
-// Pin 11   A1          grey
-// Pin 13   A2          black
-// Pin 15   CS          brown
-// Pin 17   EN_R        blue
-// Pin 18   EN_G        purple
-// Pin 19   not used
-// sonst.   GND
 /*
+Pin 01   D_G         red
+Pin 03   D_R         green
+Pin 05   CLK         orange
+Pin 07   STR         yellow
+Pin 09   A0          white
+Pin 11   A1          grey
+Pin 13   A2          black
+Pin 15   CS          brown
+Pin 17   EN_R        blue
+Pin 18   EN_G        purple
+Pin 19   not used
+sonst.   GND
+
+https://docs.arduino.cc/resources/pinouts/A000067-full-pinout.pdf
 // Section 1
 PIN_1_R D28 (PA0)
 PIN_1_G D29 (PA1)
@@ -31,23 +33,30 @@ PIN_3_G D25 (PA5)
 // Section 4
 PIN_4_R D22 (PA6)
 PIN_4_G D23 (PA7)
+
+PIN_CS D46 (PL3)
+PIN_A2 D47 (PL2)
+PIN_A1 D48 (PL1)
+PIN_A0 D49 (PL0)
 */
+
 // Global for all Sections
 #define PIN_CLK 38
 #define PIN_STR 40
 #define PIN_EN_R 11
 #define PIN_EN_G 12
 
-#define PIN_A0 46
-#define PIN_A1 48
-#define PIN_A2 50
-#define PIN_CS 52
 
 // Panel Measurements
 #define WIDTH 200
 #define HEIGHT_SECTION 16
 #define HEIGHT_ALL 64
 #define NR_OF_PXL_SECTION WIDTH*HEIGHT_SECTION
+
+// other
+#define GREEN 1
+#define RED 2
+#define YELLOW 3
 
 uint8_t bitmap[WIDTH][HEIGHT_SECTION];
 
@@ -58,30 +67,18 @@ uint8_t bitmap[WIDTH][HEIGHT_SECTION];
 3 = all 1 
 4 = file from imageData.h
 */
-void setBitmap(uint8_t mode){
-    switch (mode){
-        case 0: // off
-            memset(bitmap,0,sizeof(bitmap));
-            break;
-        case 1: // red
-            memset(bitmap,0b10101010,sizeof(bitmap));
-            break;
-        case 2: // green
-            memset(bitmap,0b01010101,sizeof(bitmap));
-            break;
-        case 3: // yellow
-            memset(bitmap,0b11111111,sizeof(bitmap));
-            break;
-        case 4: // image
-            memcpy_P(bitmap,imageData,sizeof(bitmap));
-            break; 
-        default: // pixel 1 set to red, rest off
-            memset(bitmap,0,sizeof(bitmap));
-            memset(bitmap,0b10000000,1);
-            break;
-    }
+void fillMatrix(uint8_t color){
+        uint8_t value = 0;
+        value = (color<<6) |
+                (color<<4) |
+                (color<<2) |
+                color;
+        memset(bitmap,value,sizeof(bitmap));
 }
 
+void pictureToMatrix(){
+    memcpy_P(bitmap,imageData,sizeof(bitmap));
+}
 
 // Inital setup
 void setup() {
@@ -89,21 +86,20 @@ void setup() {
     DDRA = 0xff; // set pins (PA0-PA7) output (ATmega640-1280-1281-2560-2561-Datasheet-DS40002211A.pdf, p.68)
     PORTA = 0x00; // off by default
 
+    DDRL |= 0x0f; // pins PL7, PL5, PL3, PL1 (CS, A2, A1, A0) as output
+    PORTL &= ~0x0f;  // off by default
+
     pinModeFast(PIN_CLK, OUTPUT);
     pinModeFast(PIN_STR, OUTPUT);
     pinModeFast(PIN_EN_R, OUTPUT);
     pinModeFast(PIN_EN_G, OUTPUT);
 
-    pinModeFast(PIN_A0, OUTPUT);
-    pinModeFast(PIN_A1, OUTPUT);
-    pinModeFast(PIN_A2, OUTPUT);
-    pinModeFast(PIN_CS, OUTPUT);
-
     // Inital Values
     analogWrite(PIN_EN_R, 0);  // active LOW  -> 100% Brightness = 0
     analogWrite(PIN_EN_G, 0);  //             -> 0% Brightness = 255
 
-    setBitmap(4); // bitmap variable overwritten in funtion
+    //fillMatrix(YELLOW); // bitmap variable overwritten in funtion
+    pictureToMatrix();
 }
 
 // Main Loop
@@ -115,10 +111,7 @@ void loop() {
         else yfix = y + 1;
         
         //Row selection
-        digitalWriteFast(PIN_A0, (y & 0x01) != 0);
-        digitalWriteFast(PIN_A1, (y & 0x02) != 0);
-        digitalWriteFast(PIN_A2, (y & 0x04) != 0);
-        digitalWriteFast(PIN_CS, y >= 8);
+        PORTL = (PORTL & 0xf0) | (0x0f & y); // clear bits, copy masked y into register
 
         for(int x = 0; x<WIDTH;x++){
             PORTA = bitmap[x][yfix];
@@ -130,5 +123,11 @@ void loop() {
         digitalWriteFast(PIN_STR, true);
         digitalWriteFast(PIN_STR, false);
     }
-    delay(10);
+    
+    analogWrite(PIN_EN_R, 255);  // active LOW  -> 100% Brightness = 0
+    analogWrite(PIN_EN_G, 255);  //             -> 0% Brightness = 255
+    delay(15);
+    analogWrite(PIN_EN_R, 0);  // active LOW  -> 100% Brightness = 0
+    analogWrite(PIN_EN_G, 0);  //             -> 0% Brightness = 255
+    
 }
